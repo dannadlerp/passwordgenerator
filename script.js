0bf3065ace3097b182e94b594d85a2cd85f8b7ac
dannadlerp <142226474+dannadlerp@users.noreply.github.com>
2023-10-23 14:04:59 -0400

    Add files via upload
    - fixed bug regarding unassigned password variable
    -split password variable into array
    -fixed bug where after character limit alert popped up, program did not terminate

--- script.js
+++ script.js
@@ -3,52 +3,47 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var length = password.length + 1
   passwordcompilation = ""
   const lowerCase = "abcdefghijklmnopqrstuvwxyz";
   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numeric = "0123456789"
   const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-  prompt("Please select number of characters between 8 - 128")
-  if (length < 8 > 128) {
-    alert("please select between 8 to 128")
+  length = prompt("Please select number of characters between 8 - 128");
+  if (length < 8 || length > 128) {
+    alert("please select between 8 to 128");
+    console.log("password length must be between 8 and 128 characters");
     return;
   }
-  
-  if (password < 8 > 128) {
-    alert("Password must be between 8 and 128 characters")
-    return;
-  }
-  
+    
 var wantslowercase = confirm("Would you like you use lower case letters?");
   if (wantslowercase) {
     console.log("lowercase confirmed");
-    passwordcompilation.concat(self, lowerCase)
+     passwordcompilation += lowerCase;
   }
 
 var wantsuppercase = confirm("Would you like you use upper case letters?");
   if (wantsuppercase) {
     console.log("uppercase confirmed");
-    passwordcompilation.concat(self, upperCase)
+    passwordcompilation += upperCase;
   }
 
 var wantsnumeric = confirm("Would you like you use numbers?");
   if (wantsnumeric) {
     console.log("numeric confirmed");
-    passwordcompilation.concat(self, numeric)
+    passwordcompilation += numeric;
   }
 
 var wantsspecialchar = confirm("Would you like you use special characters?");
   if (wantsspecialchar) {
     console.log("special characters confirmed");
-    passwordcompilation.concat(self, specialChar)
+    passwordcompilation += specialChar;
   }
     console.log("The password input is done")
+    document.getElementById("password").textContent = "password"
+    var splitPassword = passwordcompilation.split("");
+    console.log(splitPassword)
 }
 
-document.getElementById("password").textContent = "password"
-var passwordsplit = passwordcompilation.split(' ', length);
-console.log(passwordsplit)
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
