66e072a354da25056354b9f29e6c397c6ec441b8
dannadlerp <142226474+dannadlerp@users.noreply.github.com>
2023-10-23 21:37:09 -0400

    Add files via upload
    -fixed bug in script.js that gave htmlelementarea message in final password
    - added criteria counter to validate if password was empty
    -cited sources on readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # passwordgenerator
 Random password generator based on user input criteria
+user story:
+I want a randomly generated password based on how many characters I want, and what criteria I want.
+Acceptance criteria:
+Page loads without errors and generates password correctly based on user input criteria.
+
+
+Screenshot:
+
+URL to deployed App:
+
+Sources:
+https://stackoverflow.com/questions/44114924/javascript-password-generator-multiple-random-values-from-array
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var generatedPassword = document.getElementById("password")
 // Write password to the #password input
 function writePassword() {
   passwordcompilation = ""
@@ -8,40 +8,63 @@ function writePassword() {
   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numeric = "0123456789"
   const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var criteriaAmount = (0)
   length = prompt("Please select number of characters between 8 - 128");
+  // character length limiter
   if (length < 8 || length > 128) {
     alert("please select between 8 to 128");
     console.log("password length must be between 8 and 128 characters");
     return;
   }
-    
+   //criteria for password questions 
 var wantslowercase = confirm("Would you like you use lower case letters?");
   if (wantslowercase) {
+    criteriaAmount++
     console.log("lowercase confirmed");
      passwordcompilation += lowerCase;
   }
 
 var wantsuppercase = confirm("Would you like you use upper case letters?");
   if (wantsuppercase) {
+    criteriaAmount++
     console.log("uppercase confirmed");
     passwordcompilation += upperCase;
   }
 
 var wantsnumeric = confirm("Would you like you use numbers?");
   if (wantsnumeric) {
+    criteriaAmount++
     console.log("numeric confirmed");
     passwordcompilation += numeric;
   }
 
 var wantsspecialchar = confirm("Would you like you use special characters?");
   if (wantsspecialchar) {
+    criteriaAmount++
     console.log("special characters confirmed");
     passwordcompilation += specialChar;
   }
     console.log("The password input is done")
     document.getElementById("password").textContent = "password"
     var splitPassword = passwordcompilation.split("");
+    var passwordLength = splitPassword.length
+    generatedPassword = "" //assign to empty string before adding random characters otherwise HTML textareaelement appears
+      
+    for (i = 0; i < length; i++) {
+      var randomChar = splitPassword[Math.floor(passwordLength * Math.random())];
+      generatedPassword += randomChar;
+    }
+    //if all criteria is left blank, an error message is displayed
+    if (criteriaAmount === 0) {
+      console.log("password criteria is blank");
+        alert("Password cannot be blank");
+        generatedPassword = []
+        return;
+      }
+
+    //pass final password to html file
     console.log(splitPassword)
+    password.textContent = generatedPassword
 }
 
 
